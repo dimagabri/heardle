8441e9ef1e9d83250c2eed00ded9d3afe4dd96ce
dimagabri <gabdima@gmail.com>
2024-10-30 10:59:44 +0100

    Update main.js
    zucche

--- main.js
+++ main.js
@@ -4644,7 +4644,7 @@ var app = (function() {
         c,
         () => {
           // console.log('answer', r.length)
-          let e = "🎶Heardle is back🎶 #" + (s.id + 1),
+          let e = "🎃Heardle is back🎃 #" + (s.id + 1),
             t = "";
           a
             ?
