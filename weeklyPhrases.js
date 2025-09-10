0e19cecd54df8efbc009f8a9eb968e49994757db
dimagabri <gabdima@gmail.com>
2025-09-10 10:07:13 +0200

    Add files via upload

---
+++ weeklyPhrases.js
@@ -0,0 +1,30 @@
+export const weeklyPhrases = [
+  {
+    start: "2025-09-08",
+    end: "2025-09-14",
+    text: "Settembre è sempre difficile\n"
+  },
+  {
+    start: "2025-09-15",
+    end: "2025-09-21",
+    text: "È un fatto geografico a volte\n"
+  },
+  {
+    start: "2025-09-29",
+    end: "2025-10-05",
+    text: "Per caso c'è un concerto al Forum?\n"
+  },
+  {
+    start: "2025-10-06",
+    end: "2025-10-12",
+    text: "Abbiamo avuto anche il rap in Italia!\n"
+  },
+  {
+    start: "2025-10-13",
+    end: "2025-10-19",
+    text: "Ti ricordi quando andavamo a ballare?\n"
+  },
+  {
+    start: "2025-10-20",
+    end: "2025-10-26",
+    text:
