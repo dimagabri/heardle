d889a3323d006f7103a44505ae02556dea7ea969
dimagabri <gabdima@gmail.com>
2025-11-11 10:57:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3813,7 +3813,7 @@ var app = (function() {
       c() {
         (n = w("div")),
         (n.innerHTML =
-                        '<p class="mb-2"> Il nuovo Heardle e le canzoni non sono solo italiane! Creato con orgoglio da <a href="https://instagram.com/dimagabri">@dimagabri</a>. <p class="mb-3"> Tutti i diritti vanno ai rispettivi proprietari.<p class="mb-3"> Prova le nostre Favolose Special Week: settimane a tema, suggerite dai giocatori...oppure che rispecchiano il clima! <p class="mb-3"> Le prossime saranno: </br> - 10.11-16.11 La Disney? Davvero? </br> - 24.11-30.11 Questa me l&#39;ero scordata! </br> - Dal 01.12 al 25.12: Serve dirlo? 🎄 (Con qualche eccezione) </br> - 05.01-11.01 Mamma mia questi non li sopporto! <p class="text-xs mb-3 text-custom-line">Prepared with <a href="https://developers.soundcloud.com">SoundCloud</a>\n and powered by <a href="https://www.netlify.com//">Netlify</a>. Release 11.11.2025</br> Il gioco finirà un giorno di gennaio.'),
+                        '<p class="mb-2"> Il nuovo Heardle e le canzoni non sono solo italiane! Creato con orgoglio da <a href="https://instagram.com/dimagabri">@dimagabri</a>. <p class="mb-3"> Tutti i diritti vanno ai rispettivi proprietari.<p class="mb-3"> Prova le nostre Favolose Special Week: settimane a tema, suggerite dai giocatori...oppure che rispecchiano il clima! <p class="mb-3"> Le prossime saranno: </br> - 10.11-16.11 La Disney? Davvero? </br> - 24.11-30.11 Questa me l&#39;ero scordata! </br> - 01.12-25.12: 🎄Serve dirlo?🎄 (Con qualche eccezione) </br> - 05.01-11.01 Mamma mia questi non li sopporto! <p class="text-xs mb-3 text-custom-line">Prepared with <a href="https://developers.soundcloud.com">SoundCloud</a>\n and powered by <a href="https://www.netlify.com//">Netlify</a>. Release 11.11.2025</br> Il gioco finirà un giorno di gennaio.'),
         M(n, "class", "text");
       },
       m(e, t) {
@@ -10917,6 +10917,7 @@ var app = (function() {
     props: {},
   });
 })();
+
 
 
 
